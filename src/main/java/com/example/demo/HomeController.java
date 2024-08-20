d2dc86b732938f4cf3325c733c2856ef47f41a87
Mourya Teja Padmanabhuni <92813285+Mouryat3007@users.noreply.github.com>
2024-08-20 20:24:36 +0100

    Update HomeController.java

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -10,6 +10,6 @@ public class HomeController {
 
     @GetMapping
     public String home() {
-        return "Hello, This is create by Mourya. Oner of the repo";
+        return "Hello, This is create by Mourya. Owner of the repo";
     }
 }
