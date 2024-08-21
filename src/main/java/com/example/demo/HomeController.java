35beadef9c43af0456dea52a69a9e2f1b328fb00
Mourya Teja Padmanabhuni <92813285+Mouryat3007@users.noreply.github.com>
2024-08-21 21:24:14 +0100

    Update HomeController.java

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -10,6 +10,6 @@ public class HomeController {
 
     @GetMapping
     public String home() {
-        return "Hello, This is create by Mourya. This is triggred by github webhook whi h is CI/CD";
+        return "Hello, This is create by Mourya. This is triggred by github webhook which is CI/CD. This is deployed using docker container";
     }
 }
